76b882e17cd609aee567aab65b0521ae9ac70966
diego pastor <dieg0407@hotmail.com>
2022-12-03 17:57:04 -0500

    added automcomplition plugin

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -4,4 +4,6 @@ require('core.plugin_config.nvim-tree')
 require('core.plugin_config.telescope')
 require('core.plugin_config.treesitter')
 require('core.plugin_config.toggleterm')
-
+require('core.plugin_config.completitions')
+require('core.plugin_config.lsp_config')
+require('core.plugin_config.gitsigns')
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -18,8 +18,24 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
   use 'nvim-treesitter/nvim-treesitter'
+
+  use 'lewis6991/gitsigns.nvim'
+
+  --completition
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'rafamadriz/friendly-snippets'
+
   use {
-    'nvim-telescope/telescope.nvim',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig'
+  }
+  use {
+   'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
@@ -27,7 +43,6 @@ return require('packer').startup(function(use)
     'akinsho/toggleterm.nvim',
     tag = '*'
   }
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
